9ab34d7ae41c5609600868cf14f193f97a117d8f
nyckosleducmanage <githubmanage@nicolasleduc.com>
2025-06-06 11:37:03 +0200

    Feat: Test renewal

--- scripts/check_expiry.sh
+++ scripts/check_expiry.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 VAULT_NAME="${1?Key Vault name required}"
 
-THRESHOLD_MIN=50
-THRESHOLD_MAX=59
-MAX_RENEWALS=50
+THRESHOLD_MIN=120
+THRESHOLD_MAX=130
+MAX_RENEWALS=120
 
 mapfile -t CERTS < <(
   az keyvault certificate list \
